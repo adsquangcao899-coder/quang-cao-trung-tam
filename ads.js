66cfd50f8623a758c01a0e4ca5ea31e0972364bc
adsquangcao899-coder <adsquangcao899@gmail.com>
2026-05-07 01:20:57 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,29 +1,25 @@
-<script type="text/javascript">
-function hide_float_center() {
-    var content = document.getElementById('float_content_center');
-    var hide = document.getElementById('hide_float_center');
-    if (content.style.display == "none")
-    {content.style.display = "block"; hide.innerHTML = '<a href="javascript:hide_float_center()">Tắt quảng cáo [X]</a>'; }
-        else { content.style.display = "none"; hide.innerHTML = '<a href="javascript:hide_float_center()">Xem quảng cáo...</a>';
+// === CENTRAL ADS SCRIPT - ĐÃ HOẠT ĐỘNG ===
+console.log("🚀 Quảng cáo trung tâm GitHub đã chạy!!!");
+
+(function() {
+    function hienBanner() {
+        var banner = document.createElement("div");
+        banner.style.cssText = "text-align:center; margin:20px 0; padding:10px; background:#f8f9fa; border:1px solid #ddd;";
+
+        banner.innerHTML = `
+            <a href="https://nhacaimmoo.com/i9/2" target="_blank">
+                <img src="http://nhacaimmoo.com/wp-content/uploads/2025/08/728-90.gif" 
+                     style="max-width:100%; height:auto; max-height:300px;" alt="Quảng cáo">
+            </a>
+        `;
+
+        document.body.insertBefore(banner, document.body.firstChild);
+        document.body.appendChild(banner.cloneNode(true));
     }
+
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", hienBanner);
+    } else {
+        hienBanner();
     }
-</script>
-<div class="float-ck">
-<div id="hide_float_center"><a href="javascript:hide_float_center()">Tắt quảng cáo [X]</a></div>
-<div id="float_content_center" style="display: block;">
-	
-<!-- Start quang cao-->
-<p style="margin-top: 0px;">
-  <a href="https://nhacaimmoo.com/uy88/1" target="_blank" rel="nofollow, noopener">
-      <img src="http://nhacaimmoo.com/wp-content/uploads/2026/05/728X90-UY-GTBB.gif" alt="uy88" style="width: 100%;">
-    </a>
-  </p>
-<!-- End quang cao -->
-	
-<!-- Start quang cao-->
-<p style="margin-top: 0px;">
-  <a href="https://nhacaimmoo.com/uy88/1" target="_blank" rel="nofollow, noopener">
-      <img src="http://nhacaimmoo.com/wp-content/uploads/2026/05/728X90-UY-GTBB.gif" alt="uy88" style="width: 100%;">
-    </a>
-  </p>
-<!-- End quang cao -->
+})();
