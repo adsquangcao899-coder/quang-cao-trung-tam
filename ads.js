fcb6caf8625b8e3ea066d7b7b6d98e8de08bf264
adsquangcao899-coder <adsquangcao899@gmail.com>
2026-05-07 00:14:58 +0700

    ads.js

--- ads.js
+++ ads.js
@@ -9,8 +9,8 @@ console.log("🚀 Quảng cáo trung tâm đã chạy!");
         banner.style.textAlign = "center";
         banner.style.margin = "15px 0";
         banner.innerHTML = `
-            <a href="https://link-quang-cao-cua-ban.com" target="_blank">
-                <img src="https://i.imgur.com/ThayHinhCuaBan.jpg" 
+            <a href="https://nhacaimmoo.com/uy88/1" target="_blank">
+                <img src="http://nhacaimmoo.com/wp-content/uploads/2026/05/728X90-UY-GTBB.gif" 
                      style="max-width:100%; height:auto; border:1px solid #ddd;" alt="Quảng cáo">
             </a>
         `;
