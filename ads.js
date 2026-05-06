f804a4c1d089a8988fbd5b988fe5aa77c90fed88
adsquangcao899-coder <adsquangcao899@gmail.com>
2026-05-07 01:54:20 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -65,6 +65,8 @@ function hienFloatingBanner() {
     banner.innerHTML = `
         <a href="https://nhacaimmoo.com/i9/2" target="_blank">
             <img src="http://nhacaimmoo.com/wp-content/uploads/2025/08/728-90.gif" alt="Quảng cáo">
+        <a href="https://nhacaimmoo.com/i9/2" target="_blank">
+            <img src="http://nhacaimmoo.com/wp-content/uploads/2025/08/728-90.gif" alt="Quảng cáo">
         </a>
         <div id="hide_float_center">
             <a href="#" onclick="this.parentElement.parentElement.style.display='none'; return false;">× Đóng</a>
